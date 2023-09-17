0ec2df2db7fc3622361133dcc5dd5a5c0c4d4707
kimyunho <31121701+ua1it@users.noreply.github.com>
2023-09-17 14:09:51 +0900

    feat: styled-components 적용 및 eslint 규칙 수정

---
+++ src/app/styles.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 97.5vh;
+  background-color: #f5f8faff;
+`;
+
+export const Wrapper = styled.div`
+  width: 30%;
+  height: 90%;
+  background-color: white;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+`;
